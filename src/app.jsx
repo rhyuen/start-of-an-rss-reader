dd97c796a727b4d9390a9883de4dbf2971b682e5
Robert Yuen <dungeonry@hotmail.com>
2017-01-29 16:32:48 -0800

    Removed Source.jsx from being needed.  Everything just plops onto the main page as a series of SourceItems.

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,41 @@
 import React, {Component} from "react";
-import Source from "./source.jsx";
 import Header from "./header.jsx";
 import Banner from "./banner.jsx";
+import SourceItem from "./sourceitem.jsx";
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      content: []
     };
+    this.showResponse = this.showResponse.bind(this);
+    this.getData = this.getData.bind(this);
   }
 
+  showResponse(response){
+    // var updatedContent = this.state.content.slice(0);
+    // updatedContent.push(response.data);
 
+    this.setState({
+      content: this.state.content.concat(response.data)
+    });
+  }
 
-  render(){
+  getData(URL){
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: URL,
+      success: function(response){
+        // this.state.content.push(response);
+        this.showResponse(response);
+        console.dir(response);
+      }.bind(this)
+    });
+  }
+
+  componentDidMount(){
     const urlList = {
       "The Guardian": "/guardian",
       "Vancouver Sun": "/vsunworld",
@@ -24,13 +46,26 @@ class App extends Component {
       "CBC": "/cbcworld"
     };
 
+    for(var key in urlList){
+      this.getData(urlList[key]);
+    }
+  }
 
-    const masterSource = Object.keys(urlList).map((currSourceKey, index) => {
+
+  render(){
+    var sources = this.state.content.map((currItem, index) => {
+      var regex = /(<([^>]+)>)/ig;
+      var newdesc = currItem.description.toString().replace(regex, "");
+
       return (
-        <Source
-          key = {index}
-          sourceTitle = {currSourceKey.toString()}
-          url = {urlList[currSourceKey]}/>
+        <SourceItem
+          newsSource = {currItem.newsSource}
+          title = {currItem.title}
+          link = {currItem.link}
+          description = {newdesc}
+          pubDate = {currItem.pubDate}
+          author = {currItem.author}
+          category = {currItem.category}/>
       );
     });
 
@@ -39,11 +74,42 @@ class App extends Component {
         <Header/>
         <Banner/>
         <div className = "primary-content-container">
-          {masterSource}
+          {sources}
         </div>
       </div>
     );
   }
+  // render(){
+  //   const urlList = {
+  //     "The Guardian": "/guardian",
+  //     "Vancouver Sun": "/vsunworld",
+  //     "NY Times": "/nytimesworld",
+  //     "Washington Post": "/wapoworld",
+  //     "BBC": "/bbcworld",
+  //     "Globe and Mail": "/globemailworld",
+  //     "CBC": "/cbcworld"
+  //   };
+  //
+  //
+  //   const masterSource = Object.keys(urlList).map((currSourceKey, index) => {
+  //     return (
+  //       <Source
+  //         key = {index}
+  //         sourceTitle = {currSourceKey.toString()}
+  //         url = {urlList[currSourceKey]}/>
+  //     );
+  //   });
+  //
+  //   return (
+  //     <div className = "root">
+  //       <Header/>
+  //       <Banner/>
+  //       <div className = "primary-content-container">
+  //         {masterSource}
+  //       </div>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
